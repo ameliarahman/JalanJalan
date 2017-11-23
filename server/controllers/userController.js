4248d6723d93579e5ee6bb89664b25f5afcd2df5
setiadijoe <setiadi_y56@yahoo.co.id>
2017-11-23 21:01:32 +0700

    api signin with jwt and bcrypt

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,8 +1,11 @@
-const user = require('../models/userModel');
+const UserModel = require('../models/userModel');
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const key = process.env.JWT_SECRET
 
 class User {
   static signup(req, res) {
-    user.create({
+    UserModel.create({
       name: req.body.name,
       username: req.body.username,
       password: req.body.password,
@@ -11,6 +14,29 @@ class User {
     }).catch((err) => {
       res.send(err);
     });
+  }
+
+  static signin (req, res) {
+    UserModel.findOne({ username: req.body.username })
+      .then(userData => {
+        if (bcrypt.compareSync(req.body.password, userData.password)) {
+          let payload = {
+            id: userData._id,
+            name: userData.name,
+            username: userData.username
+          }
+          let token = jwt.sign(payload, key)
+          console.log('Sukses login');
+          res.status(201).send({ message: 'User has succesfully login', token })
+        } else {
+          console.log('gagal login');
+          res.status(401).send({ message: 'password or username not valid' })
+        }
+      })
+      .catch(err => {
+        console.log('belum daftar');
+        res.status(500).send(err)
+      })
   }
 }
 
